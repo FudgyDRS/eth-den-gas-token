acf0f5556d8125fc73cf0f1fa1b36b53f94f22e7
fudgydrs <yetimonsters@gmail.com>
2023-03-05 03:25:35 -0700

    liquidator engine

--- contracts/LiquidationEngine.sol
+++ contracts/LiquidationEngine.sol
@@ -12,14 +12,40 @@ contract LiquidationEngine is Ownable, AccessControlEnumerable {
   address _gasToken;
   address _bank;
   uint256 _redeemFee;
+  bytes32 public constant LIQUIDATOR_ROLE = keccak256("LIQUIDATOR_ROLE");
 
-  function TargetedLiquidation(address address_) public payable {
-    // get position in question
-    // talk to the bank and get the 
-    require();
+  event ApplicationForLiquidator(address account, uint256 timestamp);
+  event GrantedLiquidator(address account, uint256 timestamp);
+  event RevokedLiquidator(address account, uint256 timestamp);
+
+  function ApplyForLiquidator() public {
+    require(!hasRole(LIQUIDATOR_ROLE, msg.sender), "Role already set");
+    emit ApplicationForLiquidator(msg.sender, block.timestamp);
   }
 
-  function Redemption(uint256 amount_) public payable {
+  function GrantLiquidator(address address_) public {
+    require(!hasRole(LIQUIDATOR_ROLE, msg.sender), "Role already set");
+    grantRole(LIQUIDATOR_ROLE, address_);
+    emit GrantedLiquidator(address_, block.timestamp);
+  }
+
+  function RevokeLiquidator(address address_) public {
+    require(hasRole(LIQUIDATOR_ROLE, msg.sender), "Role not set");
+    revokeRole(LIQUIDATOR_ROLE, address_);
+    emit RevokedLiquidator(address_, block.timestamp);
+  }
+
+  function TargetedLiquidation(address address_) public payable onlyRole(LIQUIDATOR_ROLE) {
+    // get position in question
+    // talk to the bank and get the 
+    bool success;
+    bytes memory message;
+    bytes memory payload = abi.encodeWithSignature("TargetedLiquidation(address)", address_);
+    (success, message) = Bank.call(payload);
+    require(success);
+  }
+
+  function Redemption(uint256 amount_) public payable onlyRole(LIQUIDATOR_ROLE) {
       // get position(s) that satisfy the amount to be paid for amount_ at current collateral
 
   }
